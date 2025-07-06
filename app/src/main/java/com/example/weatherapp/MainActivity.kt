f6ebf6c5e1adf58abc1da288f4d75ff52b1770e3
Jerônimo Morais <jeronimo.cm@outlook.com>
2025-07-06 17:38:58 -0300

    Prática 02 - parte 02.

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,26 +1,16 @@
 package com.example.weatherapp
 
-import android.app.Activity
-import android.content.Intent
-import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.HomePage
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,23 +42,6 @@ class MainActivity : ComponentActivity() {
 //        Greeting("Android")
 //    }
 //}
-
-@Composable
-fun HomePage(modifier : Modifier = Modifier){
-    val activity = LocalActivity.current
-
-    Column ( modifier = modifier.fillMaxSize().padding(32.dp),
-        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally ) {
-
-        Text(text = "Seja bem-vindo!", fontSize = 28.sp,modifier = Modifier.padding(32.dp))
-
-        Button(onClick = {
-
-            activity?.finish()
-
-        }) { Text("Logout") }
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
