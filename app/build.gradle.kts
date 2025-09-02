1b8ba73ab39bfa5bffbe2b6ce8a4a2af12f313f1
Jerônimo Morais <jcm1@discente.ifpe.edu.br>
2025-09-02 11:27:05 -0300

    Prática 11 - parte 01.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,16 +7,17 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
     alias(libs.plugins.google.gms.google.services)
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
 }
 
 android {
     namespace = "com.example.weatherapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.weatherapp"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -26,6 +27,11 @@ android {
         val props = Properties()
         props.load (keyFile.inputStream())
         buildConfigField ("String", "WEATHER_API_KEY", props.getProperty("WEATHER_API_KEY"))
+        manifestPlaceholders["MAPS_API_KEY"] = props.getProperty("MAPS_API_KEY")
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
 
     buildTypes {
@@ -88,5 +94,9 @@ dependencies {
     implementation("com.google.maps.android:maps-compose:2.8.0")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    val room_version = "2.7.2"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
 
 }
